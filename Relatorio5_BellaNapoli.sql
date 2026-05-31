e50e909731ef3880a2fbcbc47e756935239c5e71
Nathalia Gonçalves <158405216+GoncalvesNath@users.noreply.github.com>
2026-05-30 21:24:24 -0300

    Change condition from data_entrega to data_pedido

--- Relatorio5_BellaNapoli.sql
+++ Relatorio5_BellaNapoli.sql
@@ -38,10 +38,10 @@ WHERE endereco IN ('Centro', 'Sudoeste', 'Jardins');
    AMPLIAÇÃO DO DESAFIO
    ===================================================== */
 
-/* 1. Pedidos com data_entrega nula */
+/* 1. Pedidos com data_pedido nula */
 SELECT *
 FROM pedido
-WHERE data_entrega IS NULL;
+WHERE data_pedido IS NULL;
 
 
 /* 2. Clientes que possuem pedidos (EXISTS) */
